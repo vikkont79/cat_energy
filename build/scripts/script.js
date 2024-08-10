e846d7d153f61e5c9fd30ac6d8efeabf75fd4311
vikkont79 <vvikkont79@yandex.ru>
2024-08-10 11:39:49 +0300

    Восстановление после отката коммитов

---
+++ build/scripts/script.js
@@ -0,0 +1,13 @@
+// source/scripts/script.js
+var navMain = document.querySelector(".page-header__nav");
+var navToggle = document.querySelector(".page-header__toggle");
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("page-header__nav--closed")) {
+    navMain.classList.remove("page-header__nav--closed");
+    navMain.classList.add("page-header__nav--opened");
+  } else {
+    navMain.classList.add("page-header__nav--closed");
+    navMain.classList.remove("page-header__nav--opened");
+  }
+});
+//# sourceMappingURL=script.js.map
