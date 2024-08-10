2ac36fd60bd5a833ff36b553bf4e241e931e3cd6
vikkont79 <vvikkont79@yandex.ru>
2024-08-10 12:20:56 +0300

    Удаляем папку build

--- build/scripts/script.js
+++
@@ -1,1 +0,0 @@
-var e=document.querySelector(".page-header__nav"),a=document.querySelector(".page-header__toggle");a.addEventListener("click",function(){e.classList.contains("page-header__nav--closed")?(e.classList.remove("page-header__nav--closed"),e.classList.add("page-header__nav--opened")):(e.classList.add("page-header__nav--closed"),e.classList.remove("page-header__nav--opened"))});
